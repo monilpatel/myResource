24f7f3e8abfa03d55ac17d1f5be2bc2cce0271b3
sidtuli <stuli@terpmail.umd.edu>
2015-06-30 16:14:58 -0400

    Forgot to includ searchLInk.js in last update

--- www/searchLink.js
+++ www/searchLink.js
@@ -1,17 +1,37 @@
 $(document).ready(function() {
     $("#searchBtn").click(function() {
-    	var toAdd = $("#searchValue").val();
+        /* 'Cleaning out' the old information and getting the wanted search term */
         $('p').remove();
+    	var toAdd = $("#searchValue").val();
+        
+        /* Grabbing data from the JSON on the SPL of the drug */
 		$.getJSON("https://api.fda.gov/drug/label.json?search=brand_name:"+toAdd, function(data) {
-            
 			$("#brand_name").append("<p>"+data.results[0].openfda.brand_name+"</p>");
 			$("#generic_name").append("<p>"+data.results[0].openfda.generic_name+"</p>");
-			$("#purpose").append("<p>"+data.results[0].purpose+"</p>");
+            $("#purpose").append("<p>"+data.results[0].purpose+"</p>");
 		});
         
+        /* Grabbingdata from the JSON on the adverse events on te drug */
 		$.getJSON("https://api.fda.gov/drug/event.json?search=brand_name:"+toAdd, function(data){
 			$("#side_effects").append("<p>"+data.results[0].patient.reaction[0].reactionmeddrapt+"</p>");
 		}); 
+        
+    });
+    
+    $(document).keypress(function(event) {
+        if (event.which === 13) {
+            $('p').remove();
+            var toAdd = $("#searchValue").val();
+            $.getJSON("https://api.fda.gov/drug/label.json?search=brand_name:"+toAdd, function(data) {
+
+                $("#brand_name").append("<p>"+data.results[0].openfda.brand_name+"</p>");
+                $("#generic_name").append("<p>"+data.results[0].openfda.generic_name+"</p>");
+                $("#purpose").append("<p>"+data.results[0].purpose+"</p>");
+            });
+            $.getJSON("https://api.fda.gov/drug/event.json?search=brand_name:"+toAdd, function(data){
+                $("#side_effects").append("<p>"+data.results[0].patient.reaction[0].reactionmeddrapt+"</p>");
+            }); 
+        }
     });
 });
 
