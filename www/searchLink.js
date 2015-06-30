a78e6a37e22bd3f52b56b614278d2df01ddad2bb
sidtuli <stuli@terpmail.umd.edu>
2015-06-30 15:38:35 -0400

    Searchlink.js update
    Removed text when each search is made.

--- www/searchLink.js
+++ www/searchLink.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
     $("#searchBtn").click(function() {
     	var toAdd = $("#searchValue").val();
+        $('p').remove();
 		$.getJSON("https://api.fda.gov/drug/label.json?search=brand_name:"+toAdd, function(data) {
             
 			$("#brand_name").append("<p>"+data.results[0].openfda.brand_name+"</p>");
 			$("#generic_name").append("<p>"+data.results[0].openfda.generic_name+"</p>");
 			$("#purpose").append("<p>"+data.results[0].purpose+"</p>");
 		});
+        
 		$.getJSON("https://api.fda.gov/drug/event.json?search=brand_name:"+toAdd, function(data){
 			$("#side_effects").append("<p>"+data.results[0].patient.reaction[0].reactionmeddrapt+"</p>");
 		}); 
