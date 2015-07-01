628e8d2974ef1a48ad946c6ecfd4ea4a4d6f6d7f
Monil Patel <pmonil@vt.edu>
2015-07-01 15:08:29 -0400

    Hide info
    Hide info until search is completed

--- platforms/android/assets/www/searchLink.js
+++ platforms/android/assets/www/searchLink.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
-    $("#searchBtn").click(function() {
+    var searchFDA = function() {
+         /* 'Cleaning out' the old information and getting the wanted search term */
+        $('p').remove();
     	var toAdd = $("#searchValue").val();
+        
+        /* Grabbing data from the JSON on the SPL of the drug */
 		$.getJSON("https://api.fda.gov/drug/label.json?search=brand_name:"+toAdd, function(data) {
-			
-			while($("p").length> 0) {
-				$("p").remove()
-			}
 			$("#brand_name").append("<p>"+data.results[0].openfda.brand_name+"</p>");
 			$("#generic_name").append("<p>"+data.results[0].openfda.generic_name+"</p>");
-			$("#purpose").append("<p>"+data.results[0].purpose+"</p>");
+            $("#purpose").append("<p>"+data.results[0].purpose+"</p>");
 		});
+        
+        /* Grabbingdata from the JSON on the adverse events on te drug */
 		$.getJSON("https://api.fda.gov/drug/event.json?search=brand_name:"+toAdd, function(data){
 			$("#side_effects").append("<p>"+data.results[0].patient.reaction[0].reactionmeddrapt+"</p>");
 		}); 
+    }
+    $("#searchBtn").click(function() {
+       searchFDA();
+    });
+    
+    $(document).keypress(function(event) {
+        if (event.which === 13) {
+            searchFDA();
+        }
     });
 });
 
