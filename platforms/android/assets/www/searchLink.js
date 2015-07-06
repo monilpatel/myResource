977002ef4c0a8d98a3bcbd2d18822fc5e1719c4a
sidtuli <stuli@terpmail.umd.edu>
2015-07-06 14:19:39 -0400

    Small Stuff

--- platforms/android/assets/www/searchLink.js
+++ platforms/android/assets/www/searchLink.js
@@ -1,30 +1,21 @@
 $(document).ready(function() {
-<<<<<<< Updated upstream
-    $("#toast1").show();
-=======
->>>>>>> Stashed changes
     $(".heading").hide();
     var searchFDA = function() {
-         /* 'Cleaning out' the old information and getting the wanted search term */
-        $('p').remove();
+         /* Getting the wanted search term */
     	var toAdd = $("#searchValue").val();
         
         /* Grabbing data from the JSON on the SPL of the drug */
 		$.getJSON("https://api.fda.gov/drug/label.json?search=brand_name:"+toAdd, function(data) {
-			$("#brand_name").append("<p>"+data.results[0].openfda.brand_name+"</p>");
-			$("#generic_name").append("<p>"+data.results[0].openfda.generic_name+"</p>");
-            $("#purpose").append("<p>"+data.results[0].purpose+"</p>");
+			$("#brand_name").text(data.results[0].openfda.brand_name);
+			$("#generic_name").text(data.results[0].openfda.generic_name);
+            $("#purpose").text(data.results[0].purpose);
             $(".heading").show();
-<<<<<<< Updated upstream
             
-=======
->>>>>>> Stashed changes
 		});
         
-        /* Grabbingdata from the JSON on the adverse events on te drug */
+        /* Grabbing data from the JSON on the adverse events on te drug */
 		$.getJSON("https://api.fda.gov/drug/event.json?search=brand_name:"+toAdd, function(data){
-			$("#side_effects").append("<p>"+data.results[0].patient.reaction[0].reactionmeddrapt+"</p>");
-       
+			$("#side_effects").text(data.results[0].patient.reaction[0].reactionmeddrapt);
 		}); 
     }
     $("#searchBtn").click(function() {
