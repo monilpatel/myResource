feea98fc8617302c8c40330823c40e23a71e4b85
sidtuli <stuli@terpmail.umd.edu>
2015-07-08 20:42:38 -0400

    Added more info
    Tweaked the jquery so we could grab information on the active
    ingredients, and other brands given by openFDA. Additionally, added more
    information on side effects.

--- www/searchLink.js
+++ www/searchLink.js
@@ -11,15 +11,19 @@ $(document).ready(function() {
             /* Checking if the information is in the JSON and then displaying it */
             
             /* Getting and dispaying brand name*/
-			(data.results[0].openfda.brand_name) ? brand = data.results[0].openfda.brand_name : brand = "No inormation found on brand name.";
+			(data.results[0].openfda.brand_name) ? 
+                brand = data.results[0].openfda.brand_name : brand = "No inormation found on brand name.";
             $("#brand_name").text(brand);
             
             /* Getting and displaying generic name */
-            (data.results[0].openfda.generic_name) ? generic = data.results[0].openfda.generic_name : generic = "No information found on generic name";
-			$("#generic_name").text(generic);
+            (data.results[0].openfda.generic_name) ? 
+                generic = data.results[0].openfda.generic_name : generic = "No information found on generic name";
+			
+            $("#generic_name").text(generic);
             
             /* Getting and displaying purpose */
-            (data.results[0].purpose) ? purpose = data.results[0].purpose : purpose = "No information found on purpose.";
+            (data.results[0].purpose) ? 
+                purpose = data.results[0].purpose : purpose = "No information found on purpose.";
             $("#purpose").text(purpose);
             
             $(".heading").show();
@@ -34,12 +38,38 @@ $(document).ready(function() {
         
         /* Grabbing data from the JSON on the adverse events on the drug */
 		$.getJSON("https://api.fda.gov/drug/event.json?search=brand_name:"+toAdd, function(data){
+            var brands, activeIngredient;
+            (data.results[0].patient.drug[0].openfda.brand_name[0]) ? brands = data.results[0].patient.drug[0].openfda.brand_name[0] : brands = "No information found on other brands.";
+            
+            var length = data.results[0].patient.drug[0].openfda.brand_name.length;
+            
+            for (var i = 1; i < length && i < 10; ++i) {
+                if (i === 10 || i === length) {
+                    brands += data.results[0].patient.drug[0].openfda.brand_name[i];
+                } else {
+                    brands += ","+data.results[0].patient.drug[0].openfda.brand_name[i];
+                }
+            }
+            
+            $("#other_brands").text(brands);
+            
+            (data.results[0].active_ingredient) ? activeIngredient = data.results[0].active_ingredient : activeIngredient = "No information found on active ingredients.";
+            $("#active_ingred").text(activeIngredient);
+		}); 
+        
+        $.getJSON("https://api.fda.gov/drug/event.json?search="+ toAdd +"&count=patient.reaction.reactionmeddrapt.exact", function(data){
             var sideEffect;
             
-            /* Getting and displaying Side Effects */
-            (data.results[0].patient.reaction[0].reactionmeddrapt) ? sideEffect = data.results[0].patient.reaction[0].reactionmeddrapt : sideEffect = "No information found on side effects.";
-			$("#side_effects").text(sideEffect);
-		}); 
+            (data.results[0]) ? sideEffect = data.results[0].term : sideEffect = "No information found on sideffects";
+            for(var i = 1; i < data.results.length && i < 5 ; ++i) {
+                if (i===5 || i ===data.results.length) {
+                    sideEffect += data.results[i].term;
+                } else {
+                    sideEffect += ","+data.results[i].term;
+                }
+            }
+            $("#side_effects").text(sideEffect);
+        });
     }
     /* The two ways for the user to prompt a search : clicking the search button or hitting enter */
     $("#searchBtn").click(function() {
