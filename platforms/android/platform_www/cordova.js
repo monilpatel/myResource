9c1c885cf171cf1b52460d0435a670a684ab494c
sruj0605 <srujkoneru@gmail.com>
2015-07-30 15:47:07 -0400

    js necessary for scanner

--- platforms/android/platform_www/cordova.js
+++ platforms/android/platform_www/cordova.js
@@ -1977,4 +1977,17 @@ window.cordova = require('cordova');
 
 require('cordova/init');
 
-})();
+})
+
+();
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
